c74a37b2763dab45c17b90e017c2cfe1fe82b56d
John Angarita <jnangaritaortiz@gmail.com>
2023-09-28 09:16:38 -0500

    TRELLO-12

--- src/main/java/com/foro/alura/controller/TopicosController.java
+++ src/main/java/com/foro/alura/controller/TopicosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +29,11 @@ public class TopicosController {
 	@GetMapping
 	public List<GetTopicDTO> getListTopics() {
 		return topicRepository.findAll().stream().map(GetTopicDTO::new).toList();
+	}
+
+	@GetMapping("/{id}")
+	public GetTopicDTO getTopic(@PathVariable Integer id) {
+		TopicEntity topicEntity = topicRepository.getReferenceById(id);
+		return new GetTopicDTO(topicEntity);
 	}
 }
