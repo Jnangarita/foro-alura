6cb55eafbbeac1765df7e21942b73700d40976cf
John Angarita <jnangaritaortiz@gmail.com>
2023-12-08 17:02:55 -0500

    Ajustes a la tabla usuarios

--- src/main/resources/db/migration/V2__CREATE-TABLE-USUARIOS.sql
+++ src/main/resources/db/migration/V2__CREATE-TABLE-USUARIOS.sql
@@ -1,7 +1,7 @@
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_usuario VARCHAR(25),
-    contrasena VARCHAR(300),
+    nombre_usuario VARCHAR(25) NOT NULL UNIQUE,
+    contrasena VARCHAR(300) NOT NULL,
     fecha_creacion DATE,
     usuario_activo TINYINT(1)
 );
