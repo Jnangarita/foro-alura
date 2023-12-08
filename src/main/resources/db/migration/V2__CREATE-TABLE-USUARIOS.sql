2705f2a6ba8d25fc7ca116aad73f94eaef6ac74e
John Angarita <jnangaritaortiz@gmail.com>
2023-12-08 15:37:01 -0500

    Revert "TRELLO 3 y 5"
    This reverts commit f6332c828f4d1513f599112917ca0a3068e712f8.

--- src/main/resources/db/migration/V2__CREATE-TABLE-USUARIOS.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE usuarios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_usuario VARCHAR(25) NOT NULL UNIQUE,
-    contrasena VARCHAR(300) NOT NULL,
-    fecha_creacion DATE,
-    estado TINYINT(1)
-);
